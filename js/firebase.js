01d63f9dd7e3579a687829a587da1048e0525a07
cciexamportal <dishaedu71@gmail.com>
2026-07-22 12:13:19 +0530

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,42 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCi0gz9XPJvAUUaMaGYXEC2jTQuOmkRRGM",
+  authDomain: "disha-attendance-system.firebaseapp.com",
+  projectId: "disha-attendance-system",
+  storageBucket: "disha-attendance-system.firebasestorage.app",
+  messagingSenderId: "492796083174",
+  appId: "1:492796083174:web:3bdfa2b30071c3fae07427",
+  measurementId: "G-YP9RLS7XJE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore
+const db = getFirestore(app);
+
+// Global
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
+window.getDocs = getDocs;
+window.updateDoc = updateDoc;
+window.deleteDoc = deleteDoc;
+window.doc = doc;
+window.onSnapshot = onSnapshot;
+
+console.log("Firebase Connected Successfully");
