25b559784f91c75bc0bc5bb9173351e22ae32dae
cciexamportal <dishaedu71@gmail.com>
2026-07-22 12:41:00 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,20 +1,18 @@
 function login(){
 
-let user=document.getElementById("username").value;
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
 
-let pass=document.getElementById("password").value;
+    if(user=="admin" && pass=="123456"){
 
-if(user=="admin" && pass=="123456"){
+        alert("Login Successful");
 
-alert("Login Successful");
+        window.location.href="dashboard.html";
 
-window.location.href="index.html";
+    }else{
 
-}
-else{
+        document.getElementById("msg").innerHTML="❌ Invalid Username or Password";
 
-document.getElementById("msg").innerHTML="❌ Invalid Username or Password";
-
-}
+    }
 
 }
