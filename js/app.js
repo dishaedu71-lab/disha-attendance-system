a3c5a97fcf2074cab2668df240339311ad7f5cb7
cciexamportal <dishaedu71@gmail.com>
2026-07-24 11:37:59 +0530

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,63 @@
+//================ LOADER =================
+
+let progress = 0;
+
+const bar = document.getElementById("progressBar");
+
+const percent = document.getElementById("percent");
+
+const statusText = document.getElementById("status");
+
+const msgs = [
+
+"Initializing System...",
+
+"Loading Students...",
+
+"Connecting Firebase...",
+
+"Loading Dashboard...",
+
+"Preparing Interface...",
+
+"Welcome..."
+
+];
+
+const loading = setInterval(() => {
+
+progress++;
+
+bar.style.width = progress + "%";
+
+percent.innerHTML = progress + "%";
+
+if(progress==15)
+statusText.innerHTML=msgs[1];
+
+if(progress==35)
+statusText.innerHTML=msgs[2];
+
+if(progress==60)
+statusText.innerHTML=msgs[3];
+
+if(progress==80)
+statusText.innerHTML=msgs[4];
+
+if(progress==100){
+
+statusText.innerHTML=msgs[5];
+
+clearInterval(loading);
+
+setTimeout(()=>{
+
+document.getElementById("loader").style.opacity="0";
+
+document.getElementById("loader").style.visibility="hidden";
+
+},700);
+
+}
+
+},30);
