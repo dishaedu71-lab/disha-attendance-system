82c0a66e0c25868c13febadbc9fc2af1ccc448ab
cciexamportal <dishaedu71@gmail.com>
2026-07-26 21:20:09 +0530

    Update student-login.js

--- js/student-login.js
+++ js/student-login.js
@@ -1,1 +1,62 @@
+import { auth, db } from "./firebase-config.js";
 
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    collection,
+    query,
+    where,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const studentId = document
+        .getElementById("studentId")
+        .value
+        .trim()
+        .toUpperCase();
+
+    const password = document
+        .getElementById("password")
+        .value;
+
+    try {
+
+        const q = query(
+            collection(db, "studentAccounts"),
+            where("studentId", "==", studentId)
+        );
+
+        const snapshot = await getDocs(q);
+
+        if (snapshot.empty) {
+            alert("Student ID Not Found");
+            return;
+        }
+
+        const student = snapshot.docs[0].data();
+
+        await signInWithEmailAndPassword(
+            auth,
+            student.email,
+            password
+        );
+
+        localStorage.setItem("studentId", student.studentId);
+        localStorage.setItem("studentName", student.name);
+        localStorage.setItem("studentEmail", student.email);
+
+        alert("Login Successful");
+
+        window.location.href = "student-dashboard.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+
+});
