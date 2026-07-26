e16ecfec72db5aa75fc0c04b07dd18fc18673a3d
cciexamportal <dishaedu71@gmail.com>
2026-07-26 21:45:06 +0530

    Update student-dashboard.js

--- js/student-dashboard.js
+++ js/student-dashboard.js
@@ -1,53 +1,16 @@
-import { auth, db } from "./firebase-config.js";
-
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { auth } from "./firebase-config.js";
 
 import {
-    doc,
-    getDoc
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
-const studentIdEl = document.getElementById("studentId");
-const studentNameEl = document.getElementById("studentName");
-const fatherNameEl = document.getElementById("fatherName");
-const mobileEl = document.getElementById("mobile");
-const emailEl = document.getElementById("email");
-const courseEl = document.getElementById("course");
-const batchEl = document.getElementById("batch");
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const logoutBtn = document.getElementById("logoutBtn");
 
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, (user) => {
 
     if (!user) {
         window.location.href = "student-login.html";
-        return;
-    }
-
-    try {
-
-        const snap = await getDoc(doc(db, "studentAccounts", user.uid));
-
-        if (!snap.exists()) {
-            alert("Student Record Not Found");
-            return;
-        }
-
-        const data = snap.data();
-
-        studentIdEl.textContent = data.studentId || "-";
-        studentNameEl.textContent = data.name || "-";
-        fatherNameEl.textContent = data.father || "-";
-        mobileEl.textContent = data.mobile || "-";
-        emailEl.textContent = data.email || "-";
-        courseEl.textContent = data.course || "-";
-        batchEl.textContent = data.batch || "-";
-
-    } catch (error) {
-
-        console.error(error);
-        alert(error.message);
-
     }
 
 });
