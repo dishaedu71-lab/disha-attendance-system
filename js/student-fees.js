ce58d8387641ad94eb85399e6950006a69fb2f39
cciexamportal <dishaedu71@gmail.com>
2026-07-26 22:50:02 +0530

    Update student-fees.js

--- js/student-fees.js
+++ js/student-fees.js
@@ -6,14 +6,19 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import {
-    doc,
-    getDoc
+doc,
+getDoc,
+collection,
+query,
+where,
+getDocs
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const totalFees = document.getElementById("totalFees");
 const paidFees = document.getElementById("paidFees");
 const dueFees = document.getElementById("dueFees");
 const feeStatus = document.getElementById("feeStatus");
+const paymentHistoryBody = document.getElementById("paymentHistoryBody");
 
 const logoutBtn = document.getElementById("logoutBtn");
 
@@ -39,6 +44,33 @@ onAuthStateChanged(auth, async (user) => {
         paidFees.textContent = "₹ " + (data.paidFees || 0);
         dueFees.textContent = "₹ " + (data.dueFees || 0);
         feeStatus.textContent = data.feeStatus || "Pending";
+
+        // Load Payment History
+
+const paymentQuery = query(
+    collection(db, "feesHistory"),
+    where("studentId", "==", data.studentId)
+);
+
+const paymentSnap = await getDocs(paymentQuery);
+
+paymentHistoryBody.innerHTML = "";
+
+paymentSnap.forEach((paymentDoc) => {
+
+    const payment = paymentDoc.data();
+
+    paymentHistoryBody.innerHTML += `
+    <tr>
+        <td>${payment.receiptNo}</td>
+        <td>${payment.date}</td>
+        <td>₹ ${payment.amount}</td>
+        <td>${payment.mode}</td>
+        <td>${payment.status}</td>
+    </tr>
+    `;
+
+});
 
     } catch (error) {
         console.error(error);
