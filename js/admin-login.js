3ca778da837de2f932a3c47381c40c35b79cf93b
cciexamportal <dishaedu71@gmail.com>
2026-07-26 22:20:30 +0530

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,1 +1,29 @@
+import { auth } from "./firebase-config.js";
 
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const email=document.getElementById("email");
+const password=document.getElementById("password");
+const loginBtn=document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click",async()=>{
+
+try{
+
+await signInWithEmailAndPassword(
+auth,
+email.value,
+password.value
+);
+
+window.location.href="admin-dashboard.html";
+
+}catch(error){
+
+alert(error.message);
+
+}
+
+});
