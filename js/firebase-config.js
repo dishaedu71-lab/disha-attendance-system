074085ad564cfba26a307dee148f09178779d22f
cciexamportal <dishaedu71@gmail.com>
2026-07-26 20:17:36 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,42 @@
+// Firebase SDK Import
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+// Firebase Config
+
+const firebaseConfig = {
+
+apiKey: "",
+
+authDomain: "",
+
+projectId: "",
+
+storageBucket: "",
+
+messagingSenderId: "",
+
+appId: ""
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
+// Export
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
