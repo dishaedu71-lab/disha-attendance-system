f87c3e78cf6a08ea59f3616f1158feebf39c6b2b
cciexamportal <dishaedu71@gmail.com>
2026-07-27 09:51:30 +0530

    Update admin-students.js

--- js/admin-students.js
+++ js/admin-students.js
@@ -1,3 +1,34 @@
+// ==========================
+// Generate Student ID
+// ==========================
+
+async function generateStudentId() {
+
+    const counterRef = doc(db, "counters", "studentCounter");
+
+    const studentId = await runTransaction(db, async (transaction) => {
+
+        const counterDoc = await transaction.get(counterRef);
+
+        let lastNumber = 0;
+
+        if (counterDoc.exists()) {
+            lastNumber = counterDoc.data().lastNumber;
+        }
+
+        lastNumber++;
+
+        transaction.set(counterRef, {
+            lastNumber: lastNumber
+        });
+
+        return "DCE25" + String(lastNumber).padStart(4, "0");
+
+    });
+
+    return studentId;
+
+}
 // =============================
 // Student Management
 // =============================
@@ -22,3 +53,18 @@ window.addEventListener("click", (e) => {
         studentModal.style.display = "none";
     }
 });
+const saveStudentBtn = document.getElementById("saveStudentBtn");
+
+const studentName = document.getElementById("studentName");
+const fatherName = document.getElementById("fatherName");
+const motherName = document.getElementById("motherName");
+const mobile = document.getElementById("mobile");
+const email = document.getElementById("email");
+const dob = document.getElementById("dob");
+const gender = document.getElementById("gender");
+const course = document.getElementById("course");
+const batch = document.getElementById("batch");
+const admissionDate = document.getElementById("admissionDate");
+const totalFees = document.getElementById("totalFees");
+const status = document.getElementById("status");
+const address = document.getElementById("address");
