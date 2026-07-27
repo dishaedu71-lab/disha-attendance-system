6a5e0424c754b2891041cf061b3ead3a0d4f49fb
cciexamportal <dishaedu71@gmail.com>
2026-07-27 09:36:51 +0530

    Update admin-students.js

--- js/admin-students.js
+++ js/admin-students.js
@@ -1,1 +1,24 @@
+// =============================
+// Student Management
+// =============================
 
+const studentModal = document.getElementById("studentModal");
+const addStudentBtn = document.getElementById("addStudentBtn");
+const closeModal = document.getElementById("closeModal");
+
+// Open Modal
+addStudentBtn.addEventListener("click", () => {
+    studentModal.style.display = "flex";
+});
+
+// Close Modal
+closeModal.addEventListener("click", () => {
+    studentModal.style.display = "none";
+});
+
+// Close when clicking outside
+window.addEventListener("click", (e) => {
+    if (e.target === studentModal) {
+        studentModal.style.display = "none";
+    }
+});
