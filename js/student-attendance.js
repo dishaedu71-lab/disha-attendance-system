af40817b0158c0765a37c54869493cfb14675173
cciexamportal <dishaedu71@gmail.com>
2026-07-30 12:23:50 +0530

    Update student-attendance.js

--- js/student-attendance.js
+++ js/student-attendance.js
@@ -1,1 +1,17 @@
+// Demo Data (Firebase की जगह अभी Temporary Data)
 
+document.getElementById("studentName").textContent = "Adarsh Mishra";
+
+document.getElementById("studentId").textContent = "DCE250001";
+
+document.getElementById("course").textContent = "O Level";
+
+document.getElementById("batch").textContent = "Morning Batch";
+
+document.getElementById("totalClasses").textContent = "120";
+
+document.getElementById("presentClasses").textContent = "110";
+
+document.getElementById("absentClasses").textContent = "10";
+
+document.getElementById("attendancePercent").textContent = "91.67%";
