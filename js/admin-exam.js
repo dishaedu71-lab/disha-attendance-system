12d3001999d181244a8e422bc2e15d43ed965b49
cciexamportal <dishaedu71@gmail.com>
2026-08-03 12:44:51 +0530

    Update admin-exam.js

--- js/admin-exam.js
+++ js/admin-exam.js
@@ -1,1 +1,41 @@
+window.addEventListener("firebase-ready",()=>{
 
+const db=window.db;
+
+const collection=window.collection;
+
+const addDoc=window.addDoc;
+
+document.getElementById("saveSettings").onclick=async()=>{
+
+const data={
+
+course:document.getElementById("course").value,
+
+examCode:document.getElementById("examCode").value,
+
+totalQuestions:Number(document.getElementById("totalQuestions").value),
+
+examTime:Number(document.getElementById("examTime").value),
+
+random:document.getElementById("randomQuestion").checked,
+
+active:document.getElementById("examStatus").checked,
+
+createdAt:Date.now()
+
+};
+
+await addDoc(
+
+collection(db,"exam_settings"),
+
+data
+
+);
+
+alert("Exam Settings Saved");
+
+};
+
+});
