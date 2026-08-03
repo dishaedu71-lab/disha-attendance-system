c0a0cffe2854aee91508365463d1b25e8b9b73ec
cciexamportal <dishaedu71@gmail.com>
2026-08-03 11:59:34 +0530

    Update question-bank.js

--- js/question-bank.js
+++ js/question-bank.js
@@ -1,1 +1,106 @@
+// =========================================
+// DISHA COMPUTER EDUCATION
+// QUESTION BANK
+// =========================================
 
+const QUESTION_BANK = {
+
+"O Level":[
+
+{
+
+question:"CPU का Full Form क्या है?",
+
+A:"Central Processing Unit",
+
+B:"Computer Power Unit",
+
+C:"Central Program Unit",
+
+D:"Control Processing Unit",
+
+answer:"A"
+
+},
+
+{
+
+question:"HTML का पूरा नाम क्या है?",
+
+A:"Hyper Text Markup Language",
+
+B:"High Text Machine Language",
+
+C:"Home Tool Markup Language",
+
+D:"Hyper Transfer Machine Language",
+
+answer:"A"
+
+},
+
+{
+
+question:"CSS का उपयोग किसलिए किया जाता है?",
+
+A:"Programming",
+
+B:"Design",
+
+C:"Database",
+
+D:"Network",
+
+answer:"B"
+
+}
+
+],
+
+"CCC":[
+
+{
+
+question:"Computer का मस्तिष्क किसे कहा जाता है?",
+
+A:"CPU",
+
+B:"RAM",
+
+C:"Monitor",
+
+D:"Keyboard",
+
+answer:"A"
+
+}
+
+],
+
+"Python":[
+
+{
+
+question:"Python किस प्रकार की भाषा है?",
+
+A:"Compiled",
+
+B:"Interpreted",
+
+C:"Assembly",
+
+D:"Machine",
+
+answer:"B"
+
+}
+
+],
+
+"ADCA":[],
+
+"Web Development":[],
+
+"Cyber Security":[]
+
+};
