e935d070cb7517926777d0fd81b3c813184428a7
cciexamportal <dishaedu71@gmail.com>
2026-08-03 12:27:03 +0530

    Update admin-question-bank.js

--- js/admin-question-bank.js
+++ js/admin-question-bank.js
@@ -1,1 +1,112 @@
+// ===============================
+// QUESTION BANK
+// DISHA COMPUTER EDUCATION
+// ===============================
 
+window.addEventListener("firebase-ready", () => {
+
+const db = window.db;
+const collection = window.collection;
+const addDoc = window.addDoc;
+const getDocs = window.getDocs;
+
+const saveBtn = document.getElementById("saveQuestion");
+
+const questionList = document.getElementById("questionList");
+
+
+//================ SAVE QUESTION =================
+
+saveBtn.onclick = async () => {
+
+const data = {
+
+course:document.getElementById("course").value,
+
+subject:document.getElementById("subject").value,
+
+chapter:document.getElementById("chapter").value,
+
+question:document.getElementById("question").value,
+
+A:document.getElementById("optionA").value,
+
+B:document.getElementById("optionB").value,
+
+C:document.getElementById("optionC").value,
+
+D:document.getElementById("optionD").value,
+
+answer:document.getElementById("answer").value,
+
+createdAt:Date.now()
+
+};
+
+await addDoc(
+
+collection(db,"questions"),
+
+data
+
+);
+
+alert("Question Saved Successfully");
+
+loadQuestions();
+
+};
+
+
+
+//================ LOAD QUESTION =================
+
+async function loadQuestions(){
+
+questionList.innerHTML="Loading...";
+
+const snap=await getDocs(
+
+collection(db,"questions")
+
+);
+
+questionList.innerHTML="";
+
+snap.forEach(doc=>{
+
+const q=doc.data();
+
+questionList.innerHTML+=`
+
+<div class="question-item">
+
+<h3>${q.question}</h3>
+
+<p><b>Course :</b> ${q.course}</p>
+
+<p>A : ${q.A}</p>
+
+<p>B : ${q.B}</p>
+
+<p>C : ${q.C}</p>
+
+<p>D : ${q.D}</p>
+
+<p style="color:lime">
+
+Answer : ${q.answer}
+
+</p>
+
+</div>
+
+`;
+
+});
+
+}
+
+loadQuestions();
+
+});
