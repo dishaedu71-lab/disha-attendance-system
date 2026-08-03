f66145044eba476ba806314ae339304866ae30fa
cciexamportal <dishaedu71@gmail.com>
2026-08-03 11:53:14 +0530

    Update exam.js

--- js/exam.js
+++ js/exam.js
@@ -1,1 +1,24 @@
+// ===============================
+// ONLINE EXAM HOME
+// DISHA COMPUTER EDUCATION
+// ===============================
 
+const cards = document.querySelectorAll(".course-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        const course = this.dataset.course;
+
+        // Selected Course Save
+        localStorage.setItem("selectedCourse", course);
+
+        // Go to Exam Page
+        window.location.href = "start-exam.html";
+
+    });
+
+});
