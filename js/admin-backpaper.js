885235193ee72dc5dc5632aa308774209a1722e8
cciexamportal <dishaedu71@gmail.com>
2026-08-06 15:06:52 +0530

    Update admin-backpaper.js

--- js/admin-backpaper.js
+++ js/admin-backpaper.js
@@ -1,1 +1,59 @@
+// ======================================
+// BACK PAPER ADMIN PANEL
+// ======================================
 
+window.addEventListener("firebase-ready", async () => {
+
+    const db = window.db;
+
+    const status = document.getElementById("backFormStatus");
+
+    const saveBtn = document.getElementById("saveBackStatus");
+
+    // Document Reference
+
+    const ref = window.doc(db, "portal_settings", "backpaper");
+
+    // Load Setting
+
+    try {
+
+        const snap = await window.getDoc(ref);
+
+        if (snap.exists()) {
+
+            status.checked = snap.data().active;
+
+        }
+
+    } catch (err) {
+
+        console.log(err);
+
+    }
+
+    // Save Setting
+
+    saveBtn.onclick = async () => {
+
+        try {
+
+            await window.updateDoc(ref, {
+
+                active: status.checked
+
+            });
+
+            alert("Back Paper Setting Saved");
+
+        } catch (err) {
+
+            console.log(err);
+
+            alert(err.message);
+
+        }
+
+    };
+
+});
