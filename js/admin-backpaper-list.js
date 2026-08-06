df96e1bf2424ebef2871929728b83496b321da15
cciexamportal <dishaedu71@gmail.com>
2026-08-06 17:26:18 +0530

    Update admin-backpaper-list.js

--- js/admin-backpaper-list.js
+++ js/admin-backpaper-list.js
@@ -1,1 +1,66 @@
+window.addEventListener("firebase-ready", () => {
 
+const tbody = document.getElementById("backTable");
+
+window.onSnapshot(
+
+window.collection(window.db,"back_forms"),
+
+(snapshot)=>{
+
+tbody.innerHTML="";
+
+snapshot.forEach((doc)=>{
+
+const data = doc.data();
+
+tbody.innerHTML += `
+
+<tr>
+
+<td>${data.studentName}</td>
+
+<td>${data.studentId}</td>
+
+<td>${data.course}</td>
+
+<td>${data.subject}</td>
+
+<td>${data.status}</td>
+
+<td>
+
+<button
+onclick="deleteBackForm('${doc.id}')">
+
+Delete
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+});
+
+});
+
+// =====================
+
+window.deleteBackForm = async(id)=>{
+
+if(!confirm("Delete this Form?")) return;
+
+await window.deleteDoc(
+
+window.doc(window.db,"back_forms",id)
+
+);
+
+alert("Deleted Successfully");
+
+}
