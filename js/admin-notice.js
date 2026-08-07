0cbf83dc7d73e35d132e960c8effc61941da298e
cciexamportal <dishaedu71@gmail.com>
2026-08-07 21:38:30 +0530

    Update admin-notice.js

--- js/admin-notice.js
+++ js/admin-notice.js
@@ -1,1 +1,64 @@
+window.addEventListener("firebase-ready", async()=>{
 
+const ref = window.doc(
+window.db,
+"website_notice",
+"popup"
+);
+
+const snap = await window.getDoc(ref);
+
+if(snap.exists()){
+
+const data = snap.data();
+
+document.getElementById("title").value =
+data.title || "";
+
+document.getElementById("message").value =
+data.message || "";
+
+document.getElementById("buttonText").value =
+data.buttonText || "";
+
+document.getElementById("buttonLink").value =
+data.buttonLink || "";
+
+document.getElementById("active").checked =
+data.active || false;
+
+}
+
+document.getElementById("saveNotice").onclick =
+async()=>{
+
+await window.setDoc(
+
+ref,
+
+{
+
+title:
+document.getElementById("title").value,
+
+message:
+document.getElementById("message").value,
+
+buttonText:
+document.getElementById("buttonText").value,
+
+buttonLink:
+document.getElementById("buttonLink").value,
+
+active:
+document.getElementById("active").checked
+
+}
+
+);
+
+alert("✅ Website Notice Saved");
+
+};
+
+});
