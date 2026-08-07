0db06dd2e423eb225adb39701054549ed952a1a6
cciexamportal <dishaedu71@gmail.com>
2026-08-07 17:43:53 +0530

    Update student-dashboard.js

--- js/student-dashboard.js
+++ js/student-dashboard.js
@@ -1,4 +1,15 @@
+import {
+getFirestore,
+collection,
+query,
+where,
+getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 import { auth } from "./firebase-config.js";
+import { app } from "./firebase-config.js";
+
+const db = getFirestore(app);
 
 import {
     onAuthStateChanged,
@@ -7,14 +18,38 @@ import {
 
 const logoutBtn = document.getElementById("logoutBtn");
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async(user) => {
 
     if (!user) {
+
         window.location.href = "student-login.html";
+
+        return;
+
     }
 
-});
+    const studentId =
+    localStorage.getItem("studentId");
 
+    const q = query(
+
+        collection(db,"back_forms"),
+
+        where("studentId","==",studentId)
+
+    );
+
+    const snap = await getDocs(q);
+
+    snap.forEach((doc)=>{
+
+        const data = doc.data();
+
+        console.log(data.status);
+
+    });
+
+});
 logoutBtn.addEventListener("click", async (e) => {
 
     e.preventDefault();
